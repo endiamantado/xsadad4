a9e0692709c155856f9f0e5eefc232ac1a120631
endiamantado <dolarbase@gmail.com>
2024-06-09 22:01:54 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,14 @@ from telebot import util
 import time
 from bs4 import BeautifulSoup
 from requests import Session
+from flask import Flask
+
+app = Flask(__name__)
+
+port = int(os.environ.get('PORT', 5000))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
 
 session = Session()
 
