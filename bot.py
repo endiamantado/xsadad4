90f1dc2a1df9ed3db7e229c3ea2cbc030c9cee47
endiamantado <dolarbase@gmail.com>
2024-06-11 22:10:12 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -282,7 +282,9 @@ def send_purchase_info(message):
     markup.add(types.InlineKeyboardButton("ZEAK", url="https://t.me/afanando"))
     markup.add(types.InlineKeyboardButton("Forence", url="https://t.me/ciberforence"))
 
-    bot.reply_to(message, "Para Adquirir el Acceso al Bot Contacta a Soporte", reply_markup=markup)
+    photo_path = 'comprar.png'  # Ruta de la foto
+
+    bot.send_photo(message.chat.id, open(photo_path, 'rb'), caption="Para Adquirir el Acceso al Bot Contacta a Soporte", reply_markup=markup)
 
 @bot.message_handler(commands=['add'])
 def add_user_command(message):
